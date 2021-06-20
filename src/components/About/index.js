4d3ef676e3a027edfcfadabab6c86741af6ed4d2
George <gms1980@icloud.com>
2021-06-19 19:20:47 -0700

    updated contactForm

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from 'react'
 import coverImage from "../../assets/cover/cover-image.jpg";
 
-function About () {
-    return (
-        <section className="my-5">
-  <h1 id="about">Who am I?</h1>
-  <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-  <p>Bacon ipsum dolor amet chicken turducken bacon, flank ball tip tri-tip andouille ham hock. Leberkas kevin sirloin, ribeye tenderloin flank frankfurter. Sirloin pig tail doner, chislic ribeye sausage bacon turkey strip steak. Tail buffalo pancetta pig strip steak brisket kevin, capicola ham hock. Pork loin ribeye landjaeger sausage cupim meatball swine turducken shank. Hamburger meatball filet mignon strip steak, pork chop ribeye doner sirloin swine turkey chislic. </p>
-</section>
+function About() {
 
-        
-    );
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+      </p>
+      </div>
+    </section>
+  )
 }
 
-export default About;
+export default About
